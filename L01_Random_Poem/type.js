23e8578ea51176dc9a364f33e70b65755930a0a9
eviistdl <stadelevelin@gmail.com>
2024-03-29 19:20:45 +0100

    removeWord

--- L01_Random_Poem/type.js
+++ L01_Random_Poem/type.js
@@ -11,6 +11,7 @@ var RandomPoem;
     //Schleife die Rückwärts zählt, Am Ende Wert = 1
     for (let i = subject.length; i >= 1; i--) {
         //console.log (i);
+        //Konsole greift über die Funktion auf ein Subjekt ein Prädikat und Objekt auf
         console.log(getVerse(subject, predicate, object));
     }
     function getVerse(_subject, _predicate, _object) {
@@ -19,15 +20,13 @@ var RandomPoem;
         let randomSubject = Math.floor(Math.random() * _subject.length);
         let randomPredicate = Math.floor(Math.random() * _predicate.length);
         let randomObject = Math.floor(Math.random() * _object.length);
+        //das zufällig generierte Wort aus der weiteren Auswahl ausschließen
         let removeSubject = _subject.splice(randomSubject, 1)[0];
         let removePredicate = _predicate.splice(randomPredicate, 1)[0];
         let removeObject = _object.splice(randomObject, 1)[0];
-        // Ergebnis dem Array Inhalt zuweisen
-        // let randomSubject: string = _subject[randomSubjectIndex];
-        //let randomPredicate: string = _predicate[randomPredicateIndex];
-        //let randomObject: string = _object[randomObjectIndex];
+        //console.log(randomSubject, randomPredicate, randomObject);
+        //das ausgeschlossene Wort in der Konsole ausgeben.
         let verse = removeSubject + removePredicate + removeObject;
-        //let verse: string = _subject[randomSubject] + predicate[randomPredicate]
         return (verse);
     }
 })(RandomPoem || (RandomPoem = {}));
--- L01_Random_Poem/type.ts
+++ L01_Random_Poem/type.ts
@@ -12,10 +12,12 @@ namespace RandomPoem {
     //Schleife die Rückwärts zählt, Am Ende Wert = 1
     for (let i = subject.length; i >= 1; i--){
        //console.log (i);
+
+       //Konsole greift über die Funktion auf ein Subjekt ein Prädikat und Objekt auf
         console.log(getVerse (subject, predicate, object));
     }
 
-    function getVerse(_subject:  string[], _predicate: string[], _object: string[]){
+    function getVerse(_subject:  string[], _predicate: string[], _object: string[]){ //Die Funktion greift auf die Arrays zu und weist ihnen die Eigenschaft String zu
         //console.log ("blabla")
 
         // zufälliges Array auswählen
@@ -23,18 +25,15 @@ namespace RandomPoem {
         let randomPredicate: number = Math.floor(Math.random() * _predicate.length);
         let randomObject: number = Math.floor(Math.random() * _object.length);
 
+        //das zufällig generierte Wort aus der weiteren Auswahl ausschließen
         let removeSubject: string = _subject.splice(randomSubject, 1)[0];
         let removePredicate: string = _predicate.splice(randomPredicate, 1)[0];
         let removeObject: string = _object.splice(randomObject, 1)[0];
 
-        // Ergebnis dem Array Inhalt zuweisen
-       // let randomSubject: string = _subject[randomSubjectIndex];
-        //let randomPredicate: string = _predicate[randomPredicateIndex];
-        //let randomObject: string = _object[randomObjectIndex];
+        //console.log(randomSubject, randomPredicate, randomObject);
 
+        //das ausgeschlossene Wort in der Konsole ausgeben.
         let verse: string = removeSubject + removePredicate + removeObject;
-
-        //let verse: string = _subject[randomSubject] + predicate[randomPredicate]
         
         return (verse);
         
