2c78e8c3b968281918875403db1a1a954157c8b0
eviistdl <stadelevelin@gmail.com>
2024-03-29 18:46:15 +0100

    getVerse implementiert

--- L01_Random_Poem/type.js
+++ L01_Random_Poem/type.js
@@ -2,19 +2,29 @@
 var RandomPoem;
 (function (RandomPoem) {
     //Arrays mit entsprechendem Inhalt
-    let subject = ["Adora", "Catra", "Glimmer", "Bow", "Perfuma", "Frosta"];
-    let predicate = ["rettet", "mag", "wurde verbannt aus", "zerstört", "besucht", "hasst"];
+    let subject = ["Adora ", "Catra ", "Glimmer ", "Bow ", "Perfuma ", "Frosta "];
+    let predicate = ["rettet ", "mag ", "wurde verbannt aus ", "zerstört ", "besucht ", "hasst "];
     let object = ["Bright Moon", "Crymson Waste", "Mystacor", "Etheria", "Crypto Castle", "Beast Island"];
     //console.log (subject);
     //console.log (predicate);
     //console.log (object);
-    //Schleife dir Rückwärts zählt, Am Ende Wert = 1
+    //Schleife die Rückwärts zählt, Am Ende Wert = 1
     for (let i = subject.length; i >= 1; i--) {
+        //console.log (i);
         console.log(getVerse(subject, predicate, object));
     }
     function getVerse(_subject, _predicate, _object) {
-        return ("blabla");
-        //mit math random ein array von oben auswählen
+        //console.log ("blabla")
+        // zufälliges Array auswählen
+        let randomSubjectIndex = Math.floor(Math.random() * _subject.length);
+        let randomPredicateIndex = Math.floor(Math.random() * _predicate.length);
+        let randomObjectIndex = Math.floor(Math.random() * _object.length);
+        // Ergebnis dem Array Inhalt zuweisen
+        let randomSubject = _subject[randomSubjectIndex];
+        let randomPredicate = _predicate[randomPredicateIndex];
+        let randomObject = _object[randomObjectIndex];
+        //let verse: string = _subject[randomSubject] + predicate[randomPredicate]
+        return (randomSubject + randomPredicate + randomObject);
     }
 })(RandomPoem || (RandomPoem = {}));
 //# sourceMappingURL=type.js.map
--- L01_Random_Poem/type.ts
+++ L01_Random_Poem/type.ts
@@ -1,24 +1,37 @@
 namespace RandomPoem {
 
     //Arrays mit entsprechendem Inhalt
-    let subject: string[] = ["Adora", "Catra", "Glimmer", "Bow", "Perfuma", "Frosta"];
-    let predicate: string[] = ["rettet", "mag", "wurde verbannt aus", "zerstört", "besucht", "hasst"];
+    let subject: string[] = ["Adora ", "Catra ", "Glimmer ", "Bow ", "Perfuma ", "Frosta "];
+    let predicate: string[] = ["rettet ", "mag ", "wurde verbannt aus ", "zerstört ", "besucht ", "hasst "];
     let object: string[] = ["Bright Moon", "Crymson Waste", "Mystacor", "Etheria", "Crypto Castle", "Beast Island"];
 
     //console.log (subject);
     //console.log (predicate);
     //console.log (object);
 
-    //Schleife dir Rückwärts zählt, Am Ende Wert = 1
+    //Schleife die Rückwärts zählt, Am Ende Wert = 1
     for (let i = subject.length; i >= 1; i--){
+       //console.log (i);
         console.log(getVerse (subject, predicate, object));
     }
 
     function getVerse(_subject:  string[], _predicate: string[], _object: string[]){
-        return ("blabla")
-        //mit math random ein array von oben auswählen
+        //console.log ("blabla")
+
+        // zufälliges Array auswählen
+        let randomSubjectIndex: number = Math.floor(Math.random() * _subject.length);
+        let randomPredicateIndex: number = Math.floor(Math.random() * _predicate.length);
+        let randomObjectIndex: number = Math.floor(Math.random() * _object.length);
+
+        // Ergebnis dem Array Inhalt zuweisen
+        let randomSubject: string = _subject[randomSubjectIndex];
+        let randomPredicate: string = _predicate[randomPredicateIndex];
+        let randomObject: string = _object[randomObjectIndex];
+
+        //let verse: string = _subject[randomSubject] + predicate[randomPredicate]
+        
+        return (randomSubject + randomPredicate + randomObject);
+        
     }
-
-
 
 }
